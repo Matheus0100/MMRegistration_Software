c7493c0b4dd7c31dbc56b930880c48371d157877
Matheus0100 <matheusmouradasilva2018@gmail.com>
2022-05-27 19:25:49 -0300

    Salvar

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Back_End_5.Interfaces;
 
 namespace Back_End_5.Classes
@@ -14,7 +15,25 @@ namespace Back_End_5.Classes
 
         public bool ValidarCnpj(string cnpj)
         {
-            throw new NotImplementedException();
+            //o Regex vai validar o formato da entrada dos dados.
+            if(Regex.IsMatch(cnpj, @"(^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)"))
+            {
+                if(cnpj.Length == 18)
+                {
+                    if(cnpj.Substring(11,4) == "0001")
+                    {
+                        return true;
+                    }
+                }
+                    else if(cnpj.Length == 14)
+                    {
+                        if(cnpj.Substring(8,4) == "0001")
+                        {
+                            return true;
+                        }
+                    }
+            }
+            return false;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿using Back_End_5.Classes;
-
+/*
 PessoaFisica novaPf = new PessoaFisica();
 Endereco novoEnd = new Endereco();
 
 
-//Console.WriteLine(novaPf.nome);
-//novaPf.ValidarDataNascimento("01.01.2000");
+Console.WriteLine(novaPf.nome);
+novaPf.ValidarDataNascimento("01.01.2000");
 
 novaPf.nome = "Matheus";
 novaPf.DataNascimento = "19/02/2002";
@@ -16,7 +16,7 @@ novoEnd.numero = 123;
 novoEnd.complemento = "Apartamento";
 novoEnd.endComercial = false;
 novaPf.endereco = novoEnd;
-/*
+
 Console.WriteLine($"{novaPf.nome}");
 Console.WriteLine($"{novaPf.DataNascimento}");
 Console.WriteLine($"{novaPf.cpf}");
@@ -25,10 +25,14 @@ Console.WriteLine($"{novaPf.endereco.logradouro}");
 Console.WriteLine($"{novaPf.endereco.numero}");
 Console.WriteLine($"{novaPf.endereco.complemento}");
 Console.WriteLine($"{novaPf.endereco.endComercial}");
-*/
 
 Console.WriteLine(@$"
 Nome: {novaPf.nome}
 Endereco: {novaPf.endereco.logradouro}
 Maior de idade: {novaPf.ValidarDataNascimento(novaPf.DataNascimento)}
-");
+");*/
+
+/*
+PessoaJuridica metodoPj = new PessoaJuridica();
+Console.WriteLine($"{metodoPj.ValidarCnpj("00.000.000/0002-00")}");
+*/
